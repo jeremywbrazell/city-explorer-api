075412eef72b0dc0985edacc153701b481d977e4
jeremywbrazell <jwbrazell@gmail.com>
2021-03-23 20:51:41 -0700

    pushing lab 7

--- server.js
+++ server.js
@@ -5,27 +5,36 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 app.use(cors());
-const PORT=process.env.PORT || 3002;
+const PORT = process.env.PORT || 3002;
 
 app.get('/weather', handleWeather);
-    function handleWeather(request, response) {
-try{
-  let forecastArray = weather.data.map(day => {
-    return new Forecast(day)
-  })
-  response.status(200).send(forecastArray)
-} catch (error) {
-    response.status(500).send(error.message)
-}
+
+function handleWeather(request, response) {
+    const city = weather.city_name;
+    const lat = weather.lat;
+    const lon = weather.lon;
+    try {
+        let array = weather.data.map(day => {
+            return new Forecast(day)
+        });
+        const results = {
+            city: city,
+            lat: lat,
+            lon: lon,
+            forecast: array
+        };
+        response.status(200).json(results);
+    } catch (error) {
+        response.status(500).send(error.message)
+    }
+    console.log(error)
 }
 function Forecast(obj) {
-  this.description = obj.weather.description;
-  this.date = obj.datetime;
+    this.description = obj.weather.description;
+    this.date = obj.datetime;
 }
 
-
-
 //turn on the server
-app.listen(PORT,() => {
+app.listen(PORT, () => {
     console.log(`${PORT}`)
 });
